fed012d1b01118e5b5b58361643b1b8e2e5724c5
xiezhicheng <xiezhicheng1727@126.com>
2018-06-22 18:20:29 +0800

    first commit

---
+++ build/CMakeFiles/arrayprocesssynthesis.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/xzc/kdevelop_proj/array_processing/ArrayProcessSynthesis/main.cpp" "/home/xzc/kdevelop_proj/array_processing/ArrayProcessSynthesis/build/CMakeFiles/arrayprocesssynthesis.dir/main.cpp.o"
+  "/home/xzc/kdevelop_proj/array_processing/ArrayProcessSynthesis/mic_array_processing.cpp" "/home/xzc/kdevelop_proj/array_processing/ArrayProcessSynthesis/build/CMakeFiles/arrayprocesssynthesis.dir/mic_array_processing.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/xzc/eigen3.3.4"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/arrayprocesssynthesis.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/arrayprocesssynthesis.dir/mic_array_processing.cpp.o"
+  "CMakeFiles/arrayprocesssynthesis.dir/main.cpp.o"
+  "arrayprocesssynthesis.pdb"
+  "arrayprocesssynthesis"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/arrayprocesssynthesis.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
